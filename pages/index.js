5c9431b25fb217deb09a726f955764fe4f5a3cc3
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2021-12-14 08:54:02 -1000

    feat: init 🎉

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+// Replace this file with your own code.
+
+export default function IndexPage() {
+  return (
+    <main>
+      <h1>Hello world!</h1>
+      <p>
+        Start developing your Next.js app by replacing the{" "}
+        <code>pages/index.js</code> file with your own code (this is the file
+        you are reading now).
+      </p>
+      <p>
+        To learn more about Next.js, take a look at the following resources:
+      </p>
+      <ul>
+        <li>
+          <a href="https://nextjs.org/docs" target="_blank">
+            Next.js Documentation
+          </a>{" "}
+          - learn about Next.js features and API.
+        </li>
+        <li>
+          <a href="https://nextjs.org/learn" target="_blank">
+            Learn Next.js
+          </a>{" "}
+          - an interactive Next.js tutorial.
+        </li>
+      </ul>
+    </main>
+  );
+}
